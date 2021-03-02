b2824b280f257f3096b62415c02437cfca42374c
Ekho26 <munteanu.ionut1991@gmail.com>
2021-03-02 10:40:49 +0200

    Router, Styled_components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Components
 import Chat from './components/Chat';
+import Login from './components/Login';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+
+// Styles
+import "./App.css";
+import styled from 'styled-components';
+
+// Router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/room">
-            <Chat />
-          </Route>
-          <Route path="/">
-            Login Page
-          </Route>
-        </Switch>
+        <Container>
+          <Header />
+          <Main>
+            <Sidebar />
+            <Switch>
+              <Route path="/room">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Login />
+              </Route>
+            </Switch>
+          </Main>
+        </Container>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: orange;
+  display: grid;
+  grid-template-rows: 38px auto;
+`
+
+const Main = styled.div`
+  background: blue;
+`
