4e01b663184127821e28efa334c4daa8d156f67b
Ekho26 <munteanu.ionut1991@gmail.com>
2021-03-04 18:21:48 +0200

    Customizing the message

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -5,14 +5,69 @@ import styled from 'styled-components';
 
 function ChatMessage() {
     return (
-        <Container>
-            
-        </Container>
-    )
+      <Container>
+        <UserAvatar>
+          <img src="https://randomuser.me/api/portraits/women/11.jpg" alt="Person" />
+        </UserAvatar>
+        <MessageContent>
+            <Name>
+              John
+              <span>Date</span>
+            </Name>
+          <Text>Slack message</Text>
+        </MessageContent>
+      </Container>
+    );
 }
 
 export default ChatMessage;
 
 const Container = styled.div`
+  padding: 8px 20px; /* top/bottom and right/left */
+  display: flex;
+  align-items: center;
+//   height: 35px;
+//   width: 100%;
+//   border-radius: 5px;
+//   padding: 5px;
+
+//   :hover {
+//     background: #f5f3f2;
+//   }
+`;
+
+// Users Avatars
+const UserAvatar = styled.div`
+    width: 36px;
+    height: 36px;
+    border-radius: 5px;
+    overflow: hidden;
+    margin-right: 8px;
+    
+    img {
+        width: 100%;
+    }
+`;
+
+// Messages
+const MessageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Name = styled.span`
+  font-weight: 900;
+  font-size: 15px;
+  line-height: 1.4;
+
+  span {
+    font-weight: 400;
+    color: rgb(97, 96, 97);
+    margin-left: 8px;
+    font-size: 13px;
+  }
+`;
+
+const Text = styled.span`
 
 `;
